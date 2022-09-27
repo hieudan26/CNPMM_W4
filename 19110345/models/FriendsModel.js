e4a45c064e879f1d06a9766723f4aa8d803fd553
dannguyen <hieudankt@gmail.com>
2022-09-27 22:30:11 +0700

    first commit

---
+++ 19110345/models/FriendsModel.js
@@ -0,0 +1,12 @@
+const myGroup = [
+    {
+        id: '19110345',
+        name: 'Nguyen Hieu Dan',
+    }
+]
+const idMyGroup = ['19110452', '19110345', '19110461']
+
+module.exports = {
+    myGroup,
+    idMyGroup
+}
